11586bd59d9b41b6ef277b99e69c517d81bac7ad
robertmarik <marik@mendelu.cz>
2023-02-23 18:39:40 +0100

    prejmenovani soboru

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,9 +4,6 @@ directory=../_build/html
 # touch ../intro.md
 sphinx-build -Ea ../ $directory -b html
 
-
-
-
 # # Loop over lectures do open ipynb files in Colab rather than md files.
 # echo "Fixing Colab links from md to ipynb."
 # for i in $directory/prednaska/*.html
@@ -21,24 +18,24 @@ sed -i 's/<p>\\fi<\/p>//' $directory/*/*.html
 sed -i 's/<span>Example <\/span> (Řešení)/Řešení/' $directory/cviceni/cviceni*.html
 sed -i 's/<body /<body class="cviceni" /' $directory/cviceni/cviceni*.html
 
-sed -i 's/Corollary/Důsledek/' $directory/*/slidy.html
-sed -i 's/Theorem/Věta/' $directory/*/slidy.html
-sed -i 's/Remark/Poznámka/' $directory/*/slidy.html
-sed -i 's/Definition/Definice/' $directory/*/slidy.html
-sed -i 's/Example/Příklad/' $directory/*/slidy.html
-sed -i 's/\\dm//' $directory/*/slidy.html
+sed -i 's/Corollary/Důsledek/' $directory/*/index.html
+sed -i 's/Theorem/Věta/' $directory/*/index.html
+sed -i 's/Remark/Poznámka/' $directory/*/index.html
+sed -i 's/Definition/Definice/' $directory/*/index.html
+sed -i 's/Example/Příklad/' $directory/*/index.html
+sed -i 's/\\dm//' $directory/*/index.html
 
-sed -i 's/<p><code class="docutils literal notranslate"><span class="pre">ww2:\(.*\)<\/span><\/code><\/p>/<a onclick=otevrit("\1"); target="_blank" class="WWotazka">Testová otázka<\/a>/' $directory/*/slidy.html
+sed -i 's/<p><code class="docutils literal notranslate"><span class="pre">ww2:\(.*\)<\/span><\/code><\/p>/<a onclick=otevrit("\1"); target="_blank" class="WWotazka">Testová otázka<\/a>/' $directory/*/index.html
 
-sed -i 's/<p>https:\/\/youtu.be\/\(.*\)<\/p>/<div class="Ycontainer"> <iframe class="responsive-iframe" src="https:\/\/www.youtube.com\/embed\/\1"><\/iframe> <\/div>/' $directory/*/slidy.html
+sed -i 's/<p>https:\/\/youtu.be\/\(.*\)<\/p>/<div class="Ycontainer"> <iframe class="responsive-iframe" src="https:\/\/www.youtube.com\/embed\/\1"><\/iframe> <\/div>/' $directory/*/index.html
 
-sed -i 's/<p>manimp:\(.*\)|\(.*\)<\/p>/<div class="obtekat"><figure><a href="https:\/\/user.mendelu.cz\/marik\/manim\/\1" target=_blank><img src="https:\/\/user.mendelu.cz\/marik\/manim\/thumbs\/\1.jpg"><\/a><figcaption>\2 <a href="\/marik\/manim\/\1" class=but-prez target=_blank>Otevřít prezentaci<\/a><\/figcaption><\/figure><\/div>/' $directory/*/slidy.html
+sed -i 's/<p>manimp:\(.*\)|\(.*\)<\/p>/<div class="obtekat"><figure><a href="https:\/\/user.mendelu.cz\/marik\/manim\/\1" target=_blank><img src="https:\/\/user.mendelu.cz\/marik\/manim\/thumbs\/\1.jpg"><\/a><figcaption>\2 <a href="\/marik\/manim\/\1" class=but-prez target=_blank>Otevřít prezentaci<\/a><\/figcaption><\/figure><\/div>/' $directory/*/index.html
 
-sed -i 's/<p>manim:\(.*\)|\(.*\)|\(.*\)<\/p>/<div class="obtekat"><figure><a href="https:\/\/user.mendelu.cz\/marik\/manim\/\1" target=_blank><img src="https:\/\/user.mendelu.cz\/marik\/manim\/thumbs\/\1.jpg"><\/a><figcaption>\3 <a href="https:\/\/user.mendelu.cz\/marik\/manim\/\1" class=but-prez target=_blank>Otevřít prezentaci<\/a> <a href="https:\\\\youtu.be\/\2" class=but-video target=_blank>Spustit video<\/a><\/figcaption><\/figure><\/div>/' $directory/*/slidy.html
+sed -i 's/<p>manim:\(.*\)|\(.*\)|\(.*\)<\/p>/<div class="obtekat"><figure><a href="https:\/\/user.mendelu.cz\/marik\/manim\/\1" target=_blank><img src="https:\/\/user.mendelu.cz\/marik\/manim\/thumbs\/\1.jpg"><\/a><figcaption>\3 <a href="https:\/\/user.mendelu.cz\/marik\/manim\/\1" class=but-prez target=_blank>Otevřít prezentaci<\/a> <a href="https:\\\\youtu.be\/\2" class=but-video target=_blank>Spustit video<\/a><\/figcaption><\/figure><\/div>/' $directory/*/index.html
 
-sed -i 's/<p><img alt="\(.*\)" src="\(.*\)" \/><\/p>/<p><img src="\2" \/><figcaption>\1<\/figcaption><\/p>/' $directory/*/slidy.html
+sed -i 's/<p><img alt="\(.*\)" src="\(.*\)" \/><\/p>/<p><img src="\2" \/><figcaption>\1<\/figcaption><\/p>/' $directory/*/index.html
 
-sed -i 's/<\/body>/<script>jQuery(".bd-toc").removeClass("show")<\/script><\/body>/' $directory/*/slidy.html
+sed -i 's/<\/body>/<script>jQuery(".bd-toc").removeClass("show")<\/script><\/body>/' $directory/*/index.html
 sed -i 's/<\/body>/<script>jQuery(".bd-toc").removeClass("show")<\/script><\/body>/' $directory/cviceni/cviceni*.html
 sed -i 's/<body>/<script>jQuery(".bd-toc").removeClass("show")<\/script><\/body>/' $directory/cviceni/cviceni*.html
 
