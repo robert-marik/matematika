e4429e59050b585d1585daf6194ab771e40abaeb
Robert Marik <marik@mendelu.cz>
2023-07-19 13:58:43 +0200

    odstraneni \scriptsize z html

--- scripts/build.sh
+++ scripts/build.sh
@@ -14,6 +14,9 @@ sphinx-build -Ea ../ $directory -b html
 # sed -i 's/colab.research.google.com\/github\/robert-marik\/dmp\/blob\/main\/intro\.md/colab.research.google.com\/github\/robert-marik\/dmp\/blob\/gh-pages\/_sources\/intro.ipynb/' $directory/intro.html
 
 sed -i 's/<p>\\iffalse<\/p>//' $directory/*/*.html
+sed -i 's/\\bgroup//' $directory/*/*.html
+sed -i 's/\\scriptsize//' $directory/*/*.html
+sed -i 's/\\egroup//' $directory/*/*.html
 sed -i 's/<p>\\fi<\/p>//' $directory/*/*.html
 sed -i 's/<p>\\iffalse<\/p>//' $directory/*.html
 sed -i 's/<p>\\fi<\/p>//' $directory/*.html
